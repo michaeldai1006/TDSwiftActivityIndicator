fc46aae386e4746877ead259defb009769e64a5a
Michael Dai <michaeldai@Michaels-MacBook-Pro.local>
2019-01-16 23:58:02 -0800

    TDSwiftActivityIndicator helper methods

--- tdswiftactivityindicator_example/SampleApp/ViewController.swift
+++ tdswiftactivityindicator_example/SampleApp/ViewController.swift
@@ -1,20 +1,9 @@
-//
-//  ViewController.swift
-//  tdswiftactivityindicator_example
-//
-//  Created by Michael Dai on 1/16/19.
-//  Copyright © 2019 Michael Dai. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
 }
 
--- tdswiftactivityindicator_example/TDSwiftActivityIndicator.swift
+++ tdswiftactivityindicator_example/TDSwiftActivityIndicator.swift
@@ -2,7 +2,23 @@ import Foundation
 import UIKit
 
 class TDSwiftActivityIndicator {
-    static func present(onViewController viewController: UIViewController) {
+    init() {
         
+    }
+    
+    func present(onViewController viewController: UIViewController) {
+        
+    }
+    
+    private func isViewControllerVisible(viewController: UIViewController) -> Bool {
+        return viewController.isViewLoaded && viewController.view.window != nil
+    }
+    
+    private func manageUserInteraction(userInteractionEnabled enabled: Bool) {
+        if enabled {
+            UIApplication.shared.endIgnoringInteractionEvents()
+        } else {
+            UIApplication.shared.beginIgnoringInteractionEvents()
+        }
     }
 }
