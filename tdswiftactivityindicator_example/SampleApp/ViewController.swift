a05a7167034ac4c9eb4d2a0e8bcaae0e4c64e4ab
Michael Dai <michaeldai@Michaels-MacBook-Pro.local>
2019-01-17 00:16:14 -0800

    indicatorBackgroundView

--- tdswiftactivityindicator_example/SampleApp/ViewController.swift
+++ tdswiftactivityindicator_example/SampleApp/ViewController.swift
@@ -4,6 +4,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Create indicator instance and present
+        let indicator = TDSwiftActivityIndicator()
+        indicator.present(onViewController: self)
     }
 }
 
--- tdswiftactivityindicator_example/TDSwiftActivityIndicator.swift
+++ tdswiftactivityindicator_example/TDSwiftActivityIndicator.swift
@@ -2,12 +2,26 @@ import Foundation
 import UIKit
 
 class TDSwiftActivityIndicator {
-    init() {
-        
+    
+    var backgroundWidth: CGFloat
+    
+    var indicatorBackgroundView: UIView {
+        let bgView: UIView = UIView()
+        bgView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+        bgView.backgroundColor = UIColor(red:0.27, green:0.27, blue:0.27, alpha: 0.7)
+        bgView.clipsToBounds = true
+        bgView.layer.cornerRadius = 10
+        return bgView
+    }
+    
+    init(backgroundWidth: CGFloat = 80.0) {
+        self.backgroundWidth = backgroundWidth
     }
     
     func present(onViewController viewController: UIViewController) {
-        
+        let bgView = indicatorBackgroundView
+        bgView.center = viewController.view.center
+        viewController.view.addSubview(bgView)
     }
     
     private func isViewControllerVisible(viewController: UIViewController) -> Bool {
