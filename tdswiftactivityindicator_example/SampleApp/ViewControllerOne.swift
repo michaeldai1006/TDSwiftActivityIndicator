2ccb7805756532ddae4247ab24126b3087e22501
Michael Dai <michaeldai@Michaels-MacBook-Pro.local>
2019-01-19 01:55:30 -0800

    Create ViewControllerOne

--- tdswiftactivityindicator_example/SampleApp/ViewControllerOne.swift
+++ tdswiftactivityindicator_example/SampleApp/ViewControllerOne.swift
@@ -1,12 +1,12 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewControllerOne: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         // Create indicator instance and present
-        let indicator = TDSwiftActivityIndicator()
-        indicator.present(onViewController: self)
+//        let indicator = TDSwiftActivityIndicator()
+//        indicator.present(onViewController: self)
     }
 }
 
